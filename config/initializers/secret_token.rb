41e550df32b7263d00a22e8060bbcff22062621b
Cody Hill <codyhill@bdogm.com>
2011-04-13 08:30:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseballApp::Application.config.secret_token = 'f7fe12d21c539145257cfa61d61616befead6535c9b884b86ddf43fab7724287fdf5ac5928577848e8ce4771a8d63c302d0d5a05b4e731dd9b9421b8ab284800'
